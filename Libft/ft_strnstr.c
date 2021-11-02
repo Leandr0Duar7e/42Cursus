6ece740585af4a27f19dbf1c7814e36a85488846
Leandr0Duar7e <lean_duart@hotmail.com>
2021-11-02 10:44:56 +0000

    HomeWork

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -10,4 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char
+#include <stdlib.h>
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (little == '\0')
+		return (0);
+	while (big[0] != '\0' && len > 0)
+	{
+		if (big[0] == little[0])
+		{
+			while (big[i] == little[i] && len > 0)
+			{
+				i++;
+				len--;
+			}
+			if (little[i] == '\0')
+				return (big);
+			i = 0;
+		}
+		len--;
+		big++;
+	}
+	return (0);
+}
